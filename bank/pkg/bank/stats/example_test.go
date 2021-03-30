dc8822e7c0d132f00e6b49dafd8019eface3ea04
Romiz01 <romiz@mail.ru>
2021-03-30 09:36:22 +0500

    12

---
+++ bank/pkg/bank/stats/example_test.go
@@ -0,0 +1,38 @@
+package stats
+
+import (
+	"fmt"
+
+	// " bank/pkg/bank/types"
+	"github.com/Romiz01/bank101/pkg/bank/types"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			ID:       1,
+			Amount:   100_00,
+			Category: "Mobile",
+		},
+		{
+			ID:       2,
+			Amount:   5_00,
+			Category: "Mobile",
+		},
+		{
+			ID:       3,
+			Amount:   10_00,
+			Category: "Mobile",
+		},
+		{
+			ID:       4,
+			Amount:   10_00,
+			Category: "Mobile",
+		},
+	}
+	avg := Avg(payments)
+	fmt.Println(avg)
+
+	// Output:
+	//3125
+}
